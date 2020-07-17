bc654068f26f09eebfff49db9fd6e5ae9ac02e5a
Catalin Miron <mironcatalin@gmail.com>
2020-07-17 21:15:50 +0200

    Release accordion menu tutorial source code to the users

--- App.js
+++ App.js
@@ -1,13 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import data from './data';
+import { Transition, Transitioning } from 'react-native-reanimated';
+
+const transition = (
+  <Transition.Together>
+    <Transition.In type='fade' durationMs={200} />
+    <Transition.Change />
+    <Transition.Out type='fade' durationMs={200} />
+  </Transition.Together>
+);
 
 export default function App() {
+  const [currentIndex, setCurrentIndex] = React.useState(null);
+  const ref = React.useRef();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Transitioning.View
+      ref={ref}
+      transition={transition}
+      style={styles.container}
+    >
+      <StatusBar hidden />
+      {data.map(({ bg, color, category, subCategories }, index) => {
+        return (
+          <TouchableOpacity
+            key={category}
+            onPress={() => {
+              ref.current.animateNextTransition();
+              setCurrentIndex(index === currentIndex ? null : index);
+            }}
+            style={styles.cardContainer}
+            activeOpacity={0.9}
+          >
+            <View style={[styles.card, { backgroundColor: bg }]}>
+              <Text style={[styles.heading, { color }]}>{category}</Text>
+              {index === currentIndex && (
+                <View style={styles.subCategoriesList}>
+                  {subCategories.map((subCategory) => (
+                    <Text key={subCategory} style={[styles.body, { color }]}>
+                      {subCategory}
+                    </Text>
+                  ))}
+                </View>
+              )}
+            </View>
+          </TouchableOpacity>
+        );
+      })}
+    </Transitioning.View>
   );
 }
 
@@ -15,7 +57,28 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    justifyContent: 'center',
+  },
+  cardContainer: {
+    flexGrow: 1,
+  },
+  card: {
+    flexGrow: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  heading: {
+    fontSize: 38,
+    fontWeight: '900',
+    textTransform: 'uppercase',
+    letterSpacing: -2,
+  },
+  body: {
+    fontSize: 20,
+    lineHeight: 20 * 1.5,
+    textAlign: 'center',
+  },
+  subCategoriesList: {
+    marginTop: 20,
   },
 });
---
+++ README.md
@@ -0,0 +1,34 @@
+# React Native Accordion Menu
+
+# Run on your device
+
+Snack: https://snack.expo.io/@catalinmiron/react-native-accordion-menu
+
+### Youtube tutorial
+
+[![React Native Accordion Menu Youtube tutorial](pic.png)](https://youtu.be/WyWfKUo4FtE)
+[![React Native Accordion Menu Youtube tutorial](react-native-accordion-menu.gif)](https://youtu.be/WyWfKUo4FtE)
+
+In this video tutorial we'll learn how to create an accordion animation in React Native using Transition API from React Native Reanimated library (https://github.com/software-mansion/react-native-reanimated) and flexbox styling.
+We will re-create an awesome and colourful animation done by InterfaceMarket.
+
+- Inspiration: https://interfacemarket.com/ui-kits/boston-grocery-delivery-app-ui-kit
+- GitHub: https://github.com/catalinmiron/react-native-accordion-menu
+- Reanimated: https://github.com/software-mansion/react-native-reanimated
+- Expo: https://expo.io/
+
+You can find me on:
+
+- Github: http://github.com/catalinmiron
+- Twitter: http://twitter.com/mironcatalin
+
+Wanna give me a coffe?
+
+- Paypal: mironcatalin@gmail.com
+
+In this video tutorial we'll learn how to create an accordion animation in React Native using Transition API from React Native Reanimated library (https://github.com/software-mansion/react-native-reanimated) and flexbox styling.
+We will re-create an awesome and colourful animation done by InterfaceMarket.
+
+Inspiration: https://interfacemarket.com/ui-kits/boston-grocery-delivery-app-ui-kit
+GitHub: https://github.com/catalinmiron/react-native-accordion-menu
+Reanimated: https://github.com/software-mansion/react-native-reanimated
